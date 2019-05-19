9eba1a363eacad7debb89008adb6dcfc9390717f
Jari Hämäläinen <nuumiofi@gmail.com>
2019-05-19 17:28:02 +0300

    Add loop round timing and enter wait at the end

--- build-loop.sh
+++ build-loop.sh
@@ -36,9 +36,12 @@ function log {
 
 function runit {
     cd ${LINUXDIR}
+    echo "Build INCOMPLETE" > /tmp/build-status.txt
     if make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- mrproper rockchip_linux_defconfig all -j${JOBS}; then
+        echo "Build OK" | tee /tmp/build-status.txt
         true
     else
+        echo "Build FAIL" | tee /tmp/build-status.txt
         false
     fi
 }
@@ -51,25 +54,27 @@ if ${IS_DOCKER}; then
 else
     log "Tail build loop log: tail -f ${LOGDIR}/build-loop.txt"
 fi
-sleep 1
 
 OK=0
 FAIL=0
 for I in $(seq ${ROUNDS}); do
     RSTR="$(printf "Round %3d / %3d" ${I} ${ROUNDS})"
     RFILE="$(printf "%03d" ${I})"
-    log "${RSTR} start"
     LOGFILE="${LOGDIR}/build-log-${RFILE}.txt"
 
-    runit 2>&1 | tee ${LOGFILE}
-    if [[ "${PIPESTATUS[0]}" -eq 0 ]]; then
+    # See: https://serverfault.com/a/175400
+    exec 3>&1 4>&2
+    RTIME=$(TIMEFORMAT="%R"; { time (runit 2>&1 | tee ${LOGFILE} 1>&3 2>&4;) ; } 2>&1)
+    exec 3>&- 4>&-
+    BUILD_STATUS=$(cat /tmp/build-status.txt)
+    LOGINFO=", took ${RTIME} seconds"
+    if [ "${BUILD_STATUS}" = "Build OK" ]; then
         RES="$(echo "${RSTR} OK    ")"
         OK=$((OK+1))
-        LOGINFO=""
     else
         RES="$(echo "${RSTR} FAIL  ")"
         FAIL=$((FAIL+1))
-        LOGINFO=", log: ${LOGFILE}"
+        LOGINFO="${LOGINFO}, log: ${LOGFILE}"
     fi
 
     STROK="$(printf "%3d" ${OK})"
@@ -77,3 +82,9 @@ for I in $(seq ${ROUNDS}); do
     log "$(echo "${RES} Stats: ok = ${STROK}, fail = ${STRFAIL}${LOGINFO}")"
 done
 
+log "All done!"
+# If in Docker wait for enter to exit scipt and the container.
+# Makes it easier to grab logs after completing the loop.
+if ${IS_DOCKER}; then
+    read -p "Press enter to continue"
+fi
